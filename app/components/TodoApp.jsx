8cec1a45e9b3f46b8f0937aa77ed42374706da20
Kayo Filipe Oliveira Magrini Silva <kayofilipe@hotmail.com>
2017-09-07 11:26:21 -0300

    Add static todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clear the yard'
+        },
+        {
+          id: 3,
+          text: 'Leave mail on porch'
+        },
+        {
+          id: 4,
+          text: 'Play video games'
+        }
+      ]
+    };
+  },
   render: function() {
+    var {todos} = this.state;
+    console.log(todos);
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     );
   }
---
+++ app/components/TodoList.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+  render: function() {
+    var {todos} = this.props;
+
+    // when creating multiple components,
+    // each component needs to have a unique key
+    // ... for passing every attribute of todo as separate props
+    var renderTodos = () => todos.map(
+      (todo) => <Todo key={todo.id} {...todo}/>
+    );
+
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    );
+  }
+});
+
+module.exports = TodoList;
